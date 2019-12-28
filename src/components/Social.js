51f7e232045c699685dcf4ab099bc53e8cded6e4
sebastianpenttinen <sebastian.penttinen@abo.fi>
2019-12-28 23:14:04 +0200

    added welcome text

---
+++ src/components/Social.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export class Social extends Component {
+  render() {
+    return (
+      <div>
+        <ul class="social">
+          <li>
+            <a href="#" class="fa fa-facebook" />
+            <a href="#" class="fa fa-twitter" />
+            <a href="#" class="fa fa-linkedin" />
+            <a href="#" class="fa fa-instagram" />
+            <a href="#" class="fa fa-github" />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,17 @@
-import React, { Component} from "react";
-// import {Jumbotron, Container,Row, Col} from 'react-bootstrap';
-export class Welcome extends Component{    
-
-    render(){
-        return(
-            <div >
-            <h1 className="justify-content-center" style={{display: 'flex'}}>Hello I'm Sebastian Penttinen</h1>
-            <h3 className="justify-content-center" style={{display: 'flex'}}>A Turku based software engineer</h3>
-            </div>
-        )
-    }
+import React, { Component } from "react";
+import { Social } from "./Social";
+export class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="justify-content-center" style={{ display: "flex" }}>
+          Hello I'm Sebastian Penttinen
+        </h1>
+        <h3 className="justify-content-center" style={{ display: "flex" }}>
+          A Turku based software developer
+        </h3>
+        <Social />
+      </div>
+    );
+  }
 }

b6d56f0abb51d94deaf6eb4bed5e38bf54de92e0
sebastianpenttinen <sebastian.penttinen@abo.fi>
2020-02-06 21:47:23 +0200

    added profilepic and remove Python Backend

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import { Social } from "./Social";
+import ProfilePic from "../Pictures/ProfilePic.jpeg";
+
+const imageStyle = {
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  borderRadius: "25%",
+  width: "200px",
+  paddingTop: "20px"
+};
 export class Welcome extends Component {
   render() {
     return (
       <div>
+        <img style={imageStyle} src={ProfilePic} alt="ProfilePic" />
         <h1 className="justify-content-center" style={{ display: "flex" }}>
           Hello I'm Sebastian Penttinen
         </h1>

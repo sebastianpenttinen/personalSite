a85ac8690fb28a03e39baa414137186692fe1918
sebastianpenttinen <sebastian.penttinen@abo.fi>
2020-10-11 21:23:41 +0300

    Unignored files for heroku build

---
+++ backend/build/precache-manifest.570533cf59820b0c03cf7f8ee1f582a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8aab1aa9e709e5d9bfc35b45c1b916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac23e9754c028d336df6",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a3cb26730ba3d70d0c69",
+    "url": "/static/css/main.b583e0c5.chunk.css"
+  },
+  {
+    "revision": "ac23e9754c028d336df6",
+    "url": "/static/js/2.1b5fcd8c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.1b5fcd8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3cb26730ba3d70d0c69",
+    "url": "/static/js/main.7d79b2b3.chunk.js"
+  },
+  {
+    "revision": "9fb4d9d320b985ed625f",
+    "url": "/static/js/runtime-main.f3af38e6.js"
+  },
+  {
+    "revision": "455907130263cd0e28d1d3b52bf825d2",
+    "url": "/static/media/ProfilePic.45590713.jpeg"
+  }
+]);
